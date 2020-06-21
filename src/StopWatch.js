2ebaf307936e865f5f4d9514b0377b4f16a843f9
ivantxo <ivan.ramirezf@gmail.com>
2020-06-21 20:15:58 +1000

    useState, useEffect 6 - Added timer state - Increment timer only when timer is updated

--- src/StopWatch.js
+++ src/StopWatch.js
@@ -2,19 +2,25 @@ import React, { useState, useEffect } from 'react';
 
 function StopWatch() {
   const [isOn, setIsOn] = useState(false);
+  const [timer, setTimer] = useState(0);
 
   useEffect(() => {
     let interval;
     if (isOn) {
       console.log('effect runs');
-      interval = setInterval(() => console.log('tick'), 1000);
+      interval = setInterval(
+        () => setTimer(timer + 1), 
+        1000
+      );
     }
 
     return () => clearInterval(interval);
-  }, [isOn]);
+  }, [isOn, timer]);
 
   return (
     <div>
+      { timer }&nbsp;&nbsp;
+
       {!isOn && (
         <button type="button" onClick={() => setIsOn(true)}>
           Start
