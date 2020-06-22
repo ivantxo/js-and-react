132081a3e9211d87a78a86f1a7aa3f66b5331651
ivantxo <ivan.ramirezf@gmail.com>
2020-06-23 06:38:29 +1000

    Class component data fetch 2 - Added error handling

--- src/DataFetching.js
+++ src/DataFetching.js
@@ -9,18 +9,30 @@ class DataFetching extends Component {
     this.state = {
       hits: [],
       isLoading: false,
+      error: null,
     };
   }
   
   componentDidMount() {
     this.setState({ isLoading: true });
     fetch(API + DEFAULT_QUERY)
-      .then(response => response.json())
-      .then(data => this.setState({ hits: data.hits, isLoading: false }));
+      .then(response => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          throw new Error('Something went wrong...');
+        }
+      })
+      .then(data => this.setState({ hits: data.hits, isLoading: false }))
+      .catch(error => this.setState({ error, isLoading: false}));
   }
 
   render() {
-    const { hits, isLoading } = this.state;
+    const { hits, isLoading, error } = this.state;
+
+    if (error) {
+      return <p>{ error.message }</p>
+    }
 
     if (isLoading) {
       return <p>Loading...</p>
