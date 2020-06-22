f191af4e302b633c837dd70bbda20c2d4b70e1c0
ivantxo <ivan.ramirezf@gmail.com>
2020-06-22 21:03:48 +1000

    Data fetch with hooks 2 - Added a button to perform data fetching and a dependancy on search for the effecto

--- src/DataFetchingHooks.js
+++ src/DataFetchingHooks.js
@@ -1,27 +1,39 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, Fragment } from 'react';
 import Axios from 'axios';
 
 function DataFetchingHooks() {
   const [data, setData] = useState({ hits: [] });
+  const [query, setQuery] = useState('redux');
+  const [search, setSearch] = useState('');
 
   useEffect(() => {
     const fetchData = async () => {
       const result = await Axios(
-        'https://hn.algolia.com/api/v1/search?query=redux'
+        `https://hn.algolia.com/api/v1/search?query=${search}`
       );
       setData(result.data);
     };
     fetchData();
-  }, []);
+  }, [search]);
 
   return (
-    <ul>
-      {data.hits.map(item => (
-        <li key={ item.objectID }>
-          <a href={ item.url }>{ item.title }</a>
-        </li>
-      ))}
-    </ul>
+    <Fragment>
+      <input 
+        type="text"
+        value={ query }
+        onChange={ event => setQuery(event.target.value) }
+      />&nbsp;&nbsp;
+      <button type="button" onClick={() => setSearch(query) }>
+        Search
+      </button>
+      <ul>
+        {data.hits.map(item => (
+          <li key={ item.objectID }>
+            <a href={ item.url }>{ item.title }</a>
+          </li>
+        ))}
+      </ul>
+    </Fragment>
   );
 }
 
